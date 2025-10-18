3e3f670c33e185d2ebafce028c48916fa98f9d72
Olga21-12 <olgapetrenko.brxl@gmail.com>
2025-10-18 16:47:34 +0200

    Photo: Vich_Uploadable

--- pp_backend/src/Entity/Photo.php
+++ pp_backend/src/Entity/Photo.php
@@ -5,8 +5,13 @@ namespace App\Entity;
 use App\Repository\PhotoRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+
 #[ORM\Entity(repositoryClass: PhotoRepository::class)]
 #[ORM\Table(name: "photos")]
+#[Vich\Uploadable]
 class Photo
 {
     #[ORM\Id]
@@ -16,6 +21,15 @@ class Photo
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
+
+    #[Vich\UploadableField(mapping: 'bien_image', fileNameProperty: 'imageName', size: 'imageSize')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $imageSize = null;
 
     #[ORM\ManyToOne(inversedBy: 'photos')]
     private ?Bien $bien = null;
@@ -47,5 +61,35 @@ class Photo
         $this->bien = $bien;
 
         return $this;
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageFile(?File $imageFile = null): void
+{
+    $this->imageFile = $imageFile;
+}
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName ?? 'sans_photo.png';
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageSize(): ?int
+    {
+        return $this->imageSize;
+    }
+
+    public function setImageSize(?int $imageSize): void
+    {
+        $this->imageSize = $imageSize;
     }
 }
