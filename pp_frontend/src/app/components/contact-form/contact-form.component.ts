4ffa360968061e87de9c12a9d3782a0e2bb73f65
Olga21-12 <olgapetrenko.brxl@gmail.com>
2025-10-20 11:23:37 +0200

    css form-contact

--- pp_frontend/src/app/components/contact-form/contact-form.component.ts
+++ pp_frontend/src/app/components/contact-form/contact-form.component.ts
@@ -1,15 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
+  imports: [CommonModule, FormsModule], // FormsModule permet d’utiliser les directives d’Angular pour les formulaires
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
-  @Output() formSubmitted = new EventEmitter<void>();
+  messageSent = false;
 
+  // 📬 Méthode déclenchée lors du clic sur "Envoyer"
   onSubmit() {
-    this.formSubmitted.emit();
+    this.messageSent = true;
+
+    // 🔹 Le message disparaît automatiquement après 4 secondes
+    setTimeout(() => (this.messageSent = false), 4000);
   }
 }
