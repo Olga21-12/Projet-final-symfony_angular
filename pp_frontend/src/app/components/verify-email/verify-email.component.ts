e3fbf19837738402c75b598fb68a2bd81305ba60
Olga21-12 <olgapetrenko.brxl@gmail.com>
2025-10-24 16:09:23 +0200

    Correction du flux d’inscription et de connexion

--- pp_frontend/src/app/components/verify-email/verify-email.component.ts
+++ pp_frontend/src/app/components/verify-email/verify-email.component.ts
@@ -1,44 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-verify-email',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
-  template: `
-    <div class="container mt-5 text-center">
-      <h2 *ngIf="!error">✨ Vérification en cours...</h2>
-      <p *ngIf="message" class="alert alert-success mt-3">{{ message }}</p>
-      <p *ngIf="error" class="alert alert-danger mt-3">{{ error }}</p>
-    </div>
-  `
+  imports: [CommonModule],
+  template: ``
 })
-export class VerifyEmailComponent implements OnInit {
-  loading = true;
-  message = '';
-  error = '';
 
+export class VerifyEmailComponent implements OnInit {
+  
   constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {}
 
   ngOnInit() {
-    // Récupère le token depuis l’URL
     const token = this.route.snapshot.queryParamMap.get('token');
     if (token) {
-      this.http.get(`https://127.0.0.1:8000/verify/email?token=${encodeURIComponent(token)}`, { responseType: 'text' }).subscribe({
-        next: (res: any) => {
-          this.message = '✅ Votre adresse e-mail a été vérifiée avec succès !';
-      this.error = '';
-      // Перенаправляем на страницу логина через 3 секунды
-      setTimeout(() => this.router.navigate(['/login']), 2000);
+      this.http.get(`https://127.0.0.1:8000/api/verify-email?token=${encodeURIComponent(token)}`).subscribe({
+        next: () => {
+          // сохраняем флаг
+          localStorage.setItem('emailVerified', 'true');
+          // сразу редиректим на логин
+          this.router.navigate(['/login']);
         },
-        error: (err) => {
-          this.error = err.error.error || 'Lien invalide ou expiré.';
+        error: () => {
+          this.router.navigate(['/login']);
         }
       });
-    } else {
-      this.error = 'Aucun token fourni.';
     }
   }
-}
+} 
--- pp_frontend/src/app/pages/login/login.component.ts
+++ pp_frontend/src/app/pages/login/login.component.ts
@@ -1,24 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   email = '';
   password = '';
-  message = '';
   error = '';
 
   constructor(private router: Router, private http: HttpClient) { }
+
+  ngOnInit() {
+    
+  }
 
     login(){
       this.http.post('https://127.0.0.1:8000/api/login', {
@@ -26,18 +29,13 @@ export class LoginComponent {
       password: this.password
     }).subscribe({
       next: (res: any) => {
-        this.message = res.message;
+        
         this.error = '';
         localStorage.setItem('user', JSON.stringify(res.user));
-
-        // Accéder à la page de profil
-        setTimeout(() => {
           this.router.navigate(['/profile']);
-        }, 1000);
-      },
+        },
       error: (err) => {
         this.error = err.error.error || 'Erreur de connexion';
-        this.message = '';
       }
     });
   }
