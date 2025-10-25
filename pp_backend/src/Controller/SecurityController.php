f65371f074480a96d09a9154cc4d9403b6958363
Olga21-12 <olgapetrenko.brxl@gmail.com>
2025-10-25 12:14:42 +0200

    admin etape 2

--- pp_backend/src/Controller/SecurityController.php
+++ pp_backend/src/Controller/SecurityController.php
@@ -12,17 +12,23 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        // если пользователь уже вошёл — перенаправляем по роли
+    if ($this->getUser()) {
+        if (in_array('ROLE_ADMIN', $this->getUser()->getRoles(), true)) {
+            return $this->redirectToRoute('admin'); // на панель EasyAdmin
+        }
+        // если не админ — можно отправить на главную
+        return $this->redirectToRoute('app_home');
     }
+
+    $error = $authenticationUtils->getLastAuthenticationError();
+    $lastUsername = $authenticationUtils->getLastUsername();
+
+    return $this->render('security/login.html.twig', [
+        'last_username' => $lastUsername,
+        'error' => $error,
+    ]);
+}
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
